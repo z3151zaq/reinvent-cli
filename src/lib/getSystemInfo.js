a54125e4b3f5f5b4a7f69d11198edf7ade800ea3
wrx012 <rx0wang@gmail.com>
2025-07-19 18:29:54 +1200

    feat:add systeminfo

---
+++ src/lib/getSystemInfo.js
@@ -0,0 +1,30 @@
+import os from 'os';
+
+/**
+ * Get system information
+ * @returns {Object} System information object
+ */
+function getSystemInfo() {
+  try {
+    const systemInfo = {
+      platform: os.platform(),
+      arch: os.arch(),
+      hostname: os.hostname(),
+      type: os.type(),
+      release: os.release(),
+      uptime: os.uptime(),
+      totalMemory: os.totalmem(),
+      freeMemory: os.freemem(),
+      cpus: os.cpus().length,
+      userInfo: os.userInfo(),
+      networkInterfaces: os.networkInterfaces()
+    };
+
+    return systemInfo;
+  } catch (error) {
+    console.error('Error getting system info:', error);
+    throw new Error('Failed to get system information');
+  }
+}
+
+export default getSystemInfo; 
